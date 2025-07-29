c98e4535c63537e1774d46c9078f73e82a400940
raudhatul21ti <raudhatul21ti@mahasiswa.pcr.ac.id>
2025-07-29 21:58:37 +0700

    update data

--- resources/views/dokter/rekamMedis/index.blade.php
+++ resources/views/dokter/rekamMedis/index.blade.php
@@ -60,32 +60,34 @@
                                 <td>{{ $data->obat }}</td>
                                 <td>{{ $data->terapis->nama }}</td>
                                 <td class="text-center">
-                                     <!-- -- Detail -- -->
-                                            <a href="{{ route('dokter.rekamMedis.show', $data->pasien->no_rm) }}"
-                                    class="text-primary" title="Lihat Detail">
-                                    <i class="fas fa-eye"></i>
-                                    </a> 
+                                    <div class="d-flex justify-content-center gap-2">
+                                        <!-- Detail -->
+                                        <a href="{{ route('dokter.rekamMedis.show', $data->pasien->no_rm) }}"
+                                            class="btn btn-sm btn-outline-primary" title="Lihat Detail">
+                                            <i class="fas fa-eye"></i>
+                                        </a>
 
-                                    <!-- -- Edit -- -->
-                                    <a href="{{ route('dokter.rekamMedis.edit', ['rekamMedis' => $data->id]) }}"
-                                        class="text-success ml-2" title="Edit">
-                                        <i class="fas fa-edit"></i>
-                                    </a>
+                                        <!-- Edit -->
+                                        <a href="{{ route('dokter.rekamMedis.edit', ['rekamMedis' => $data->id]) }}"
+                                            class="btn btn-sm btn-outline-success" title="Edit">
+                                            <i class="fas fa-edit"></i>
+                                        </a>
 
-                                    <!-- -- Delete -- -->
-                                    <a href="#" onclick="confirmDelete('{{ $data->id }}')" class="text-danger ml-2"
-                                        title="Delete">
-                                        <i class="fas fa-trash"></i>
-                                    </a>
+                                        <!-- Delete -->
+                                        <a href="#" onclick="confirmDelete('{{ $data->id }}')"
+                                            class="btn btn-sm btn-outline-danger" title="Hapus">
+                                            <i class="fas fa-trash"></i>
+                                        </a>
 
-                                    <form id="deleteForm{{ $data->id }}"
-                                        action="{{ route('dokter.rekamMedis.destroy', ['rekamMedis' => $data->id]) }}"
-                                        method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" style="display: none;"></button>
-                                    </form>
+                                        <form id="deleteForm{{ $data->id }}"
+                                            action="{{ route('dokter.rekamMedis.destroy', ['rekamMedis' => $data->id]) }}"
+                                            method="POST" class="d-none">
+                                            @csrf
+                                            @method('DELETE')
+                                        </form>
+                                    </div>
                                 </td>
+
                             </tr>
                         @endforeach
                     </tbody>
